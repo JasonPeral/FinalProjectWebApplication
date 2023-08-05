0a4e7df3ec8cde2fa2f6e7b103a821e3fb52f8a9
Jason Peral <jasonperal@Jasons-Air.ht.home>
2023-08-04 20:38:40 -0400

    Fixed post login bug

--- DistributionCenterApp/src/main/java/com/finalProject/DistributionCenterApp/config/WebConfig.java
+++ DistributionCenterApp/src/main/java/com/finalProject/DistributionCenterApp/config/WebConfig.java
@@ -11,7 +11,8 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
-        registry.addViewController("/").setViewName("authentication/login");
+        registry.addViewController("/login");
+        registry.addViewController("/").setViewName("login.html");
     }
 
 
